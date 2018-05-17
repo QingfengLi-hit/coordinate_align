495e4d556c52806a7be2de34394b0faaa050ec56
QingfengLi-hit <747613680@qq.com>
2018-05-17 21:38:33 +0800

    add plot trajectory

--- src/absolute_orientation.cpp
+++ src/absolute_orientation.cpp
@@ -4,6 +4,11 @@
 #include<fstream>
 #include <iomanip>
 #include <Eigen/Eigenvalues>
+
+#include <stdio.h>
+#include <math.h>
+#include <limits>
+#include <sys/stat.h>
 using namespace std;
  void Coordinate_Align::input_datas(const string &keyframe_pose,const string &groundtruth_pose)
  {
@@ -166,6 +171,7 @@ void Coordinate_Align::compute_p(const double &s,const Eigen::Quaterniond &q,con
     ff << fixed;
     p=rr_ave-s*q.toRotationMatrix()*rl_ave;
     cout<<"p= "<<p<<endl;
+    double error_sum =0,total_error_ave;
     for(int i=0;i<n;i++)
       {
        key_dataset[i].position=s*q.toRotationMatrix()*key_dataset[i].position+p;
@@ -179,5 +185,142 @@ void Coordinate_Align::compute_p(const double &s,const Eigen::Quaterniond &q,con
 
        ff<< setprecision(6) <<key_dataset[i].time<< " " << setprecision(9)<<error_x<<" "
           <<error_y<<" "<<error_z<<" "<<error_ave<<endl;
+       error_sum += error_ave;
       }
+     total_error_ave = error_sum/n;
+     ff<<"total average error : "<<total_error_ave<<endl;
+     cout<<"total_error_ave = "<<total_error_ave<<endl;
+  plot_trajectory(key_dataset,some_ground_dataset);
+}
+void Coordinate_Align::plot_trajectory(const vector<PoseDatas> &key_dataset, const vector<PoseDatas> &ground_dataset)
+{
+    Matrix tmp_pose (3,1);
+    vector<Matrix> poses_result, poses_gt;
+   for(auto &keyframe_pose: key_dataset)
+   {
+       tmp_pose.val[0][0] = keyframe_pose.position(0);
+       tmp_pose.val[1][0] = keyframe_pose.position(1);
+       tmp_pose.val[2][0] = keyframe_pose.position(2);
+       poses_result.push_back(tmp_pose);
+   }
+   for(auto &ground_pose: ground_dataset)
+   {
+       tmp_pose.val[0][0] = ground_pose.position(0);
+       tmp_pose.val[1][0] = ground_pose.position(1);
+       tmp_pose.val[2][0] = ground_pose.position(2);
+       poses_gt.push_back(tmp_pose);
+   }
+  //create file
+   char * error_dir  = "errors";
+   char * plot_path_dir  =  "plot_path";
+   char * plot_error_dir =  "plot_error";
+   mkdir(error_dir, 0777);
+   mkdir(plot_path_dir, 0777);
+   mkdir (plot_error_dir, 0777);
+   file_name = "00.txt";
+
+   // check for errors
+   if (poses_gt.size()==0 || poses_result.size()!=poses_gt.size())
+   {
+     cout<<"poses_gt.size()= "<<poses_gt.size()<<endl;
+     cout<<"poses_result.size()= "<<poses_result.size()<<endl;
+     cout<< "ERROR: Could not read (all) poses of:" << file_name << endl;
+     return;
+   }
+   //save trajectory
+   savePathPlot(poses_gt,poses_result, string(plot_path_dir) + "/" + file_name);
+   //compute roi
+   vector<int32_t> roi = computeRoi(poses_gt,poses_result);
+   //plot trajectory
+   plotPathPlot(string(plot_path_dir),roi,0);
+
+}
+void Coordinate_Align::savePathPlot (vector<Matrix> &poses_gt,vector<Matrix> &poses_result,string file_name)
+{
+  // open file
+  FILE *fp = fopen(file_name.c_str(),"w");
+  // save x/z coordinates of all frames to file
+  for (int i=0; i<poses_gt.size(); i++)
+    fprintf(fp,"%f %f %f %f\n",poses_gt[i].val[0][0],poses_gt[i].val[1][0], poses_result[i].val[0][0],poses_result[i].val[1][0]);
+  // close file
+  fclose(fp);
+}
+vector<int32_t> Coordinate_Align::computeRoi (vector<Matrix> &poses_gt,vector<Matrix> &poses_result) {
+
+  float x_min = numeric_limits<int32_t>::max();
+  float x_max = numeric_limits<int32_t>::min();
+  float z_min = numeric_limits<int32_t>::max();
+  float z_max = numeric_limits<int32_t>::min();
+
+  for (vector<Matrix>::iterator it=poses_gt.begin(); it!=poses_gt.end(); it++) {
+    float x = it->val[0][1];
+    float z = it->val[1][0];
+    if (x<x_min) x_min = x; if (x>x_max) x_max = x;
+    if (z<z_min) z_min = z; if (z>z_max) z_max = z;
+  }
+
+  for (vector<Matrix>::iterator it=poses_result.begin(); it!=poses_result.end(); it++) {
+    float x = it->val[0][0];
+    float z = it->val[1][0];
+    if (x<x_min) x_min = x; if (x>x_max) x_max = x;
+    if (z<z_min) z_min = z; if (z>z_max) z_max = z;
+  }
+
+  float dx = 1.2*(x_max-x_min);
+  float dz = 1.2*(z_max-z_min);
+  float mx = 0.6*(x_max+x_min);
+  float mz = 0.6*(z_max+z_min);
+  float r  = 0.6*max(dx,dz);
+
+  vector<int32_t> roi;
+  roi.push_back((int32_t)(mx-r));
+  roi.push_back((int32_t)(mx+r));
+  roi.push_back((int32_t)(mz-r));
+  roi.push_back((int32_t)(mz+r));
+  return roi;
+}
+void Coordinate_Align::plotPathPlot (string dir,vector<int32_t> &roi,int32_t idx) {
+
+  // gnuplot file name
+  char command[1024];
+  char file_name[256];
+  sprintf(file_name,"%02d.gp",idx);
+  string full_name = dir + "/" + file_name;
+  // create png + eps
+  for (int32_t i=0; i<2; i++) {
+
+    // open file
+    FILE *fp = fopen(full_name.c_str(),"w");
+    // save gnuplot instructions
+    if (i==0) {
+      fprintf(fp,"set term png size 900,900\n");
+      fprintf(fp,"set output \"%02d.png\"\n",idx);
+    } else {
+      fprintf(fp,"set term postscript eps enhanced color\n");
+      fprintf(fp,"set output \"%02d.eps\"\n",idx);
+    }
+    fprintf(fp,"set size ratio -1\n");
+    fprintf(fp,"set xrange [%d:%d]\n",roi[0],roi[1]);
+    fprintf(fp,"set yrange [%d:%d]\n",roi[2],roi[3]);
+    fprintf(fp,"set xlabel \"x [m]\"\n");
+    fprintf(fp,"set ylabel \"z [m]\"\n");
+    fprintf(fp,"plot \"%02d.txt\" using 1:2 lc rgb \"#FF0000\" title 'Ground Truth' w lines,",idx);
+    fprintf(fp,"\"%02d.txt\" using 3:4 lc rgb \"#0000FF\" title 'Visual Odometry' w lines,",idx);
+    fprintf(fp,"\"< head -1 %02d.txt\" using 1:2 lc rgb \"#000000\" pt 4 ps 1 lw 2 title 'Sequence Start' w points\n",idx);
+
+    // close file
+    fclose(fp);
+
+    // run gnuplot => create png + eps
+    sprintf(command,"cd %s; gnuplot %s",dir.c_str(),file_name);
+    system(command);
+  }
+
+  // create pdf and crop
+  sprintf(command,"cd %s; ps2pdf %02d.eps %02d_large.pdf",dir.c_str(),idx,idx);
+  system(command);
+  sprintf(command,"cd %s; pdfcrop %02d_large.pdf %02d.pdf",dir.c_str(),idx,idx);
+  system(command);
+  sprintf(command,"cd %s; rm %02d_large.pdf",dir.c_str(),idx);
+  system(command);
 }
